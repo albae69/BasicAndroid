c020b9166873318c129efe308ecea58fd4578e34
albae69 <ahmadalbaihaqi69@gmail.com>
2021-02-04 07:13:05 +0700

    Toast

--- app/src/main/java/com/baedev/myapplication/MainActivity.kt
+++ app/src/main/java/com/baedev/myapplication/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.RadioButton
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_toast.*
 import kotlin.math.log
 
 
@@ -13,20 +15,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnOrder.setOnClickListener {
-            var food = rgFood.checkedRadioButtonId
-            var selected = findViewById<RadioButton>(food)
-            var cheese = cbCheese.isChecked
-            var onions = cbOnions.isChecked
-            var sauce = cbSauce.isChecked
-
-            var orderedBurger = "You order a burger with: \n" +
-                    "${selected.text}" +
-                    (if (cheese) "\nCheese" else "") +
-                    (if (onions) "\nOnions" else "") +
-                    (if (sauce) "\nSauce" else "")
-
-            tvOrder.text = orderedBurger
+        btnShowToast.setOnClickListener {
+            Toast(this).apply {
+                duration = Toast.LENGTH_LONG
+                view = layoutInflater.inflate(R.layout.custom_toast, clToast)
+                show()
+            }
         }
 
     }
