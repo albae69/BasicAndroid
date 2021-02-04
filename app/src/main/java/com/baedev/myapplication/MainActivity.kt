99c904bab05b8003f25d5383c6d1473940dfc42f
albae69 <ahmadalbaihaqi69@gmail.com>
2021-02-04 17:00:42 +0700

    permission android

--- app/src/main/java/com/baedev/myapplication/MainActivity.kt
+++ app/src/main/java/com/baedev/myapplication/MainActivity.kt
@@ -1,36 +1,72 @@
 package com.baedev.myapplication
 
-import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.RadioButton
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.custom_toast.*
-import kotlin.math.log
-
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnApply.setOnClickListener {
-            Intent(this, SecondActivity::class.java).also {
-                val name = etName.text.toString()
-                val age = etAge.text.toString().toInt()
-                val country = etCountry.text.toString()
-//
-//                it.putExtra("EXTRA_NAME", name)
-//                it.putExtra("EXTRA_AGE", age)
-//                it.putExtra("EXTRA_COUNTRY", country)
+        btnRequestPermission.setOnClickListener {
+            requestPermissions()
+        }
+    }
+
+    private fun writeExternalStorage() = ActivityCompat.checkSelfPermission(
+        this,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun foregroundLocation() = ActivityCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun backgroundLocation() = ActivityCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
 
 
-                val person = Person(name,age,country)
-                it.putExtra("EXTRA_PERSON",person)
-                startActivity(it)
+    private fun requestPermissions() {
+        var permissionToRequest = mutableListOf<String>()
+        if (!writeExternalStorage()) {
+            permissionToRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+        if (!foregroundLocation()) {
+            permissionToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+        if (!backgroundLocation()) {
+            permissionToRequest.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
+        if (permissionToRequest.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionToRequest.toTypedArray(), 0)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 0 && grantResults.isNotEmpty()) {
+            for (i in grantResults.indices) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "permission granted", Toast.LENGTH_LONG).show()
+                }
             }
         }
     }
+
 }
--- app/src/main/java/com/baedev/myapplication/Person.kt
+++
@@ -1,9 +0,0 @@
-package com.baedev.myapplication
-
-import java.io.Serializable
-
-data class Person(
-    val name: String,
-    val age: Int,
-    val country: String
-) : Serializable
