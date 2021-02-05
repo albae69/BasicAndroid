e6100c67bdadaae3db5d0276c2027413bccce812
albae69 <ahmadalbaihaqi69@gmail.com>
2021-02-05 16:13:28 +0700

    recylcerview

--- app/src/main/java/com/baedev/myapplication/MainActivity.kt
+++ app/src/main/java/com/baedev/myapplication/MainActivity.kt
@@ -1,36 +1,36 @@
 package com.baedev.myapplication
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.RadioButton
-import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.custom_toast.*
-import kotlin.math.log
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnApply.setOnClickListener {
-            Intent(this, SecondActivity::class.java).also {
-                val name = etName.text.toString()
-                val age = etAge.text.toString().toInt()
-                val country = etCountry.text.toString()
-//
-//                it.putExtra("EXTRA_NAME", name)
-//                it.putExtra("EXTRA_AGE", age)
-//                it.putExtra("EXTRA_COUNTRY", country)
+        var todoList = mutableListOf(
+            Todo("Learn Kotlin", true),
+            Todo(
+                "Feed Cat", false
+            ),
+            Todo("Help Mom", true),
+            Todo(
+                "live a life", true
+            )
+        )
 
+        val adapter = TodoAdapter(todoList)
+        rvTodos.adapter = adapter
+        rvTodos.layoutManager = LinearLayoutManager(this)
 
-                val person = Person(name,age,country)
-                it.putExtra("EXTRA_PERSON",person)
-                startActivity(it)
-            }
+        btnAdd.setOnClickListener {
+            val title = etAddTodo.text.toString()
+            val todo = Todo(title, false)
+            todoList.add(todo)
+            adapter.notifyItemInserted(todoList.size - 1)
+            etAddTodo.text.clear()
         }
     }
 }
---
+++ app/src/main/java/com/baedev/myapplication/Todo.kt
@@ -0,0 +1,6 @@
+package com.baedev.myapplication
+
+data class Todo(
+    val title: String,
+    var isChecked: Boolean
+)
