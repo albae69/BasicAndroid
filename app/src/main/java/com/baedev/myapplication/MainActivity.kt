ad15c391d5d67f41b1aa543e4e6733a74966f350
albae69 <ahmadalbaihaqi69@gmail.com>
2021-02-05 09:17:25 +0700

    toolbar

--- app/src/main/java/com/baedev/myapplication/MainActivity.kt
+++ app/src/main/java/com/baedev/myapplication/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.baedev.myapplication
 
-import android.app.Activity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,20 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnChoosePhoto.setOnClickListener {
-            Intent(Intent.ACTION_GET_CONTENT).also {
-                it.type = "image/*"
-                startActivityForResult(it, 0)
-            }
-        }
-
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == 0) {
-            val uri = data?.data
-            ivPhoto.setImageURI(uri)
-        }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar_menu, menu)
+        return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.miSettings -> Toast.makeText(this, "Settings touched", Toast.LENGTH_SHORT).show()
+            R.id.miFavorite -> Toast.makeText(this, "Favorite touched", Toast.LENGTH_SHORT).show()
+            R.id.miExit -> finish()
+        }
+        return true
+    }
+
 }
